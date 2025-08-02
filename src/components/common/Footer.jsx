1f971894e7ba1492eadc1b11cfd9b2c684b82a0f
rituranjankumar <kunal50639@gmail.com>
2025-08-02 14:43:09 +0530

    minor change

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -172,7 +172,7 @@ const Footer = () => {
             })}
           </div>
 
-          <div className="text-center">Made with ❤️ CodeHelp © 2023 Studynotion</div>
+          <div className="text-center">Made with ❤️ Rituranjan Kumar</div>
         </div>
       </div>
     </div>
