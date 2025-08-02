95a9c9d48fec8c792c43bc28d1b3ea365b096f23
rituranjankumar <kunal50639@gmail.com>
2025-08-02 14:24:07 +0530

    minor change

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import { useState } from 'react';
 import { useSelector } from 'react-redux'
 import { FiMenu, FiX } from 'react-icons/fi';
 import { Outlet } from 'react-router-dom';
 import Sidebar from '../components/core/Dashboard/Sidebar';
+import useOnClickOutside from '../hooks/useOnClickOutside';
 
 const Dashboard = () => {
 
@@ -11,6 +12,9 @@ const Dashboard = () => {
 
 const profileLoading=useSelector((state)=> state.profile.loading);
 const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+const dashRef=useRef(null);
+
+useOnClickOutside(dashRef,()=>setIsSidebarOpen(false))
 
   const toggleSidebar = () => {
     setIsSidebarOpen(!isSidebarOpen);
@@ -23,18 +27,20 @@ if(profileLoading || authLoading)
          
     )
 }
+
   return (
    <div className='relative flex min-h-[calc(100vh-3.5rem)]'>
       {/* Mobile Toggle Button (visible only on small screens) */}
       <button
         onClick={toggleSidebar}
-        className={`fixed z-50 sm:hidden top-15 left-2 p-2 rounded-md bg-richblack-700 text-white`}
+        className={`fixed z-30 sm:hidden top-15 left-2 p-2 rounded-md bg-richblack-700 text-white`}
       >
-        {isSidebarOpen ? <FiMenu size={24} /> : <FiMenu size={24} />}
+        <FiMenu size={24} />  
       </button>
 
       {/* Sidebar */}
       <div 
+      ref={dashRef}
         className={`flex transition-all duration-300 ${isSidebarOpen ? 'translate-x-0' : '-translate-x-full'} 
                    fixed sm:relative z-40 h-[calc(100vh-3.5rem)] sm:h-auto sm:translate-x-0`}
       >
